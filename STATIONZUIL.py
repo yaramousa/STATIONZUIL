76b10406b3bb9860016206690f8fbb2d1365b733
Yara <yara.mousa@student.hu.nl>
2023-10-24 19:21:08 +0200

    stationzuil1

--- STATIONZUIL.py
+++ STATIONZUIL.py
@@ -1,12 +1,55 @@
+import csv
 import datetime
 import random
-BERICHT = input("Voer uw bericht in  ")
+bericht = input("Voer uw bericht in:")
+if len(bericht) > 140:
+       print ('u kunt helaas niet meer schrijven')
 tijd = datetime.datetime.now()
-naam = input("Voer uw naam in ")
+print(tijd)
+naam = input("Voer uw naam in: ")
 if not naam:
-            naam = 'aanoniem'
-stations = ['utrecht','enschede','henglo']
-station = random.choice(stations)
+       print ('aanoniem')
+station = ['enschede','utrecht','henglo']
+stations = random.choice(station)
+print(stations)
+veldnamen = ['bericht','naam', 'tijd','station','beoordeling']
+bestand = open('Ns-bericht.csv', 'w')
+writer = csv.DictWriter(bestand, veldnamen)
+writer.writeheader()
+writer.writerow(
+       {'bericht': bericht,
+        'naam': naam,
+        'tijd': tijd,
+        'station': stations,
+        })
+beoordeling =input('goedegekeurd of afgekeurd')
+tid= datetime.datetime.now()
+k= input('voer je naam van mod:')
+j=input('voer je email van mod:')
+
+if beoordeling == 'goedgekeurd':
+
+
+
+    y=open('Ns-berichten','r')
+    writer = csv.DictWriter(bestand,veldnamen)
+    writer.writerow({
+          'beoordeling': beoordeling
+    })
+    print('als is in de bestand')
+else:
+    print('er is niks in de bestand opgeslan')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
